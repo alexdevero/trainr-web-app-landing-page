47babdbc746354ef10dece47b354c56cd32cd1ff
Alex Devero <deveroalex@gmail.com>
2019-11-14 09:35:51 +0100

    Add faq section with structure

---
+++ components/faq.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react'
+
+const Faq = () => (
+  <section>
+    <div className="container">
+      <h1>FAQ</h1>
+
+      <p>Your frequently asked questions answered.</p>
+
+      <h2></h2>
+      <p></p>
+
+      <h2></h2>
+      <p></p>
+
+      <h2></h2>
+      <p></p>
+
+      <h2></h2>
+      <p></p>
+
+      <h2></h2>
+      <p></p>
+
+      <h2></h2>
+      <p></p>
+    </div>
+  </section>
+)
+
+export default Faq
