191dd30e2326278accbf1ca1b14ec13055ed6cf1
Alex Devero <deveroalex@gmail.com>
2019-11-14 09:37:03 +0100

    Add structure for featured in

--- components/featured-in.tsx
+++ components/featured-in.tsx
@@ -1,7 +1,23 @@
 import * as React from 'react'
 
 const FeaturedIn = () => (
-  <section></section>
+  <section>
+    <div className="container">
+      <div className="row">
+        <div className="col-2"><img src="" alt="" className="logo"/></div>
+
+        <div className="col-2"><img src="" alt="" className="logo"/></div>
+
+        <div className="col-2"><img src="" alt="" className="logo"/></div>
+
+        <div className="col-2"><img src="" alt="" className="logo"/></div>
+
+        <div className="col-2"><img src="" alt="" className="logo"/></div>
+
+        <div className="col-2"><img src="" alt="" className="logo"/></div>
+      </div>
+    </div>
+  </section>
 )
 
 export default FeaturedIn
