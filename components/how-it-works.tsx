a899f2b2a857a83c624a315cc6b7ba4e246a281b
Alex Devero <deveroalex@gmail.com>
2019-11-14 09:23:55 +0100

    Prepare structure for how it works

---
+++ components/how-it-works.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react'
+
+const HowItWorks = () => (
+  <section>
+    <div className="container">
+      <h1>How it Works</h1>
+
+      <h2>Explain how your product or service works. </h2>
+
+      <div className="row">
+        <div className="col-2">
+          <div className="row">
+            <div className="col-6">
+              <h3></h3>
+
+              <p></p>
+            </div>
+
+            <div className="col-6">
+              <h3></h3>
+
+              <p></p>
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-6">
+              <h3></h3>
+
+              <p></p>
+            </div>
+
+            <div className="col-6">
+              <h3></h3>
+
+              <p></p>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-2">
+          <img src="" alt=""/>
+        </div>
+      </div>
+    </div>
+  </section>
+)
+
+export default HowItWorks
