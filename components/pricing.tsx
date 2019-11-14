6224ec87bdd5a3d500178552c65e5512702cbad5
Alex Devero <deveroalex@gmail.com>
2019-11-14 15:49:23 +0100

    Add intermediate plan

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -6,7 +6,7 @@ const Pricing = () => (
       <h1>Pricing</h1>
 
       <div className="row">
-        <div className="col-4">
+        <div className="col-3">
           <div className="pricing-card">
             <div className="pricing-card__header">
               <h2>&doll;19<span className="price-label">per month</span></h2>
@@ -30,14 +30,14 @@ const Pricing = () => (
           </div>
         </div>
 
-        <div className="col-4">
+        <div className="col-3">
           <div className="pricing-card">
             <div className="pricing-card__header">
-              <h2>&doll;49<span className="price-label">per month</span></h2>
+              <h2>&doll;29<span className="price-label">per month</span></h2>
             </div>
 
             <div className="pricing-card__body">
-              <h2>Professional Plan</h2>
+              <h2>Intermediate Plan</h2>
 
               <p></p>
 
@@ -49,15 +49,39 @@ const Pricing = () => (
             </div>
 
             <div className="pricing-card__footer">
-              <button>Start with Professional</button>
+              <button>Start with Intermediate</button>
             </div>
           </div>
         </div>
 
-        <div className="col-4">
+        <div className="col-3">
           <div className="pricing-card">
             <div className="pricing-card__header">
-              <h2>&doll;89<span className="price-label">per month</span></h2>
+              <h2>&doll;79<span className="price-label">per month</span></h2>
+            </div>
+
+            <div className="pricing-card__body">
+              <h2>Athlete Plan</h2>
+
+              <p></p>
+
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+
+            <div className="pricing-card__footer">
+              <button>Start with Athlete</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-3">
+          <div className="pricing-card">
+            <div className="pricing-card__header">
+              <h2>&doll;99<span className="price-label">per month</span></h2>
             </div>
 
             <div className="pricing-card__body">
