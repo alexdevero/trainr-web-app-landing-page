bfa44592d0582632949de0f4bad2e459c8983919
Alex Devero <deveroalex@gmail.com>
2019-11-14 09:33:16 +0100

    Add footer structure

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react'
 
 const Footer = () => (
-  <footer></footer>
+  <footer>
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">FAQs</a></li>
+      <li><a href="">How to use</a></li>
+      <li><a href="">About</a></li>
+      <li><a href="">Terms</a></li>
+      <li><a href="">Sign up</a></li>
+      <li><a href="">Sign in</a></li>
+      <li><a href="">Contact</a></li>
+    </ul>
+
+    <p>Copyright &copy; 2019 Devero Technologies</p>
+  </footer>
 )
 
 export default Footer
