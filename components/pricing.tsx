355beb0c9bfaf0fa14afeae27bcd4252dade9e7a
Alex Devero <deveroalex@gmail.com>
2019-11-14 09:29:46 +0100

    Prepare structure for pricing

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -1,9 +1,85 @@
 import * as React from 'react'
 
-const Pricing = () => {
-  return (
-    <div></div>
-  )
-}
+const Pricing = () => (
+  <section>
+    <div className="container">
+      <h1>Pricing</h1>
+
+      <div className="row">
+        <div className="col-4">
+          <div className="pricing-card">
+            <div className="pricing-card__header">
+              <h2>&doll;19<span className="price-label">per month</span></h2>
+            </div>
+
+            <div className="pricing-card__body">
+              <h2>Starter Plan</h2>
+
+              <p></p>
+
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+
+            <div className="pricing-card__footer">
+              <button>Start with Starter</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="pricing-card">
+            <div className="pricing-card__header">
+              <h2>&doll;49<span className="price-label">per month</span></h2>
+            </div>
+
+            <div className="pricing-card__body">
+              <h2>Professional Plan</h2>
+
+              <p></p>
+
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+
+            <div className="pricing-card__footer">
+              <button>Start with Professional</button>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-4">
+          <div className="pricing-card">
+            <div className="pricing-card__header">
+              <h2>&doll;89<span className="price-label">per month</span></h2>
+            </div>
+
+            <div className="pricing-card__body">
+              <h2>Trainer Plan</h2>
+
+              <p></p>
+
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+
+            <div className="pricing-card__footer">
+              <button>Start with Trainer</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+)
 
 export default Pricing
