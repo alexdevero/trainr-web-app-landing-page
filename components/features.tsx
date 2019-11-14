979668f8d5e1b800ba2ab2b8873dea0f7d69e268
Alex Devero <deveroalex@gmail.com>
2019-11-14 09:23:30 +0100

    Prepare structure for features

--- components/features.tsx
+++ components/features.tsx
@@ -1,9 +1,53 @@
 import * as React from 'react'
 
-const Features = () => {
-  return (
-    <div></div>
-  )
-}
+const Features = () => (
+  <section>
+    <div className="container">
+      <h1>Features/Benefits List</h1>
+
+      <h2>This is the Features/Benefits block. Write about your product or service.</h2>
+
+      <div className="row">
+        <div className="col-4">
+          <h3></h3>
+
+          <p></p>
+        </div>
+
+        <div className="col-4">
+          <h3></h3>
+
+          <p></p>
+        </div>
+
+        <div className="col-4">
+          <h3></h3>
+
+          <p></p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-4">
+          <h3></h3>
+
+          <p></p>
+        </div>
+
+        <div className="col-4">
+          <h3></h3>
+
+          <p></p>
+        </div>
+
+        <div className="col-4">
+          <h3></h3>
+
+          <p></p>
+        </div>
+      </div>
+    </div>
+  </section>
+)
 
 export default Features
