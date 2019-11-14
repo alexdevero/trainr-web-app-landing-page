6e642171f1838366245336c43c2dbdd0cd1bd798
Alex Devero <deveroalex@gmail.com>
2019-11-14 09:23:47 +0100

    Prepare structure for hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,9 +1,29 @@
 import * as React from 'react'
 
-const Hero = () => {
-  return (
-    <div></div>
-  )
-}
+const Hero = () => (
+  <section>
+    <div className="container">
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <div className="content">
+              <h1 className="hero__title"></h1>
+
+              <h2 className="hero__subtitle"></h2>
+            </div>
+          </div>
+
+          <div className="col-6">
+            <img src="" alt=""/>
+          </div>
+        </div>
+
+        <style jsx>{`
+          .hero__title {}
+        `}</style>
+      </div>
+    </div>
+  </section>
+)
 
 export default Hero
