4953da37247a65c44de65cf204655d242b8ef1d9
Alex Devero <deveroalex@gmail.com>
2019-12-04 11:15:06 +0100

    Restyle navigation

--- components/nav.tsx
+++ components/nav.tsx
@@ -29,33 +29,56 @@ const Nav = () => (
           </li>
         ))}
       </ul>
-
-      <style jsx>{`
-        :global(body) {
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir, Helvetica, sans-serif;
-        }
-        nav {
-          text-align: center;
-        }
-        ul {
-          display: flex;
-          justify-content: space-between;
-        }
-        nav > ul {
-          padding: 4px 16px;
-        }
-        li {
-          display: flex;
-          padding: 6px 8px;
-        }
-        a {
-          color: #067df7;
-          text-decoration: none;
-          font-size: 13px;
-        }
-      `}</style>
     </nav>
+
+    <style jsx>{`
+      header {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        background: #fff;
+        box-shadow: 0px 3px 12px 0px rgba(0,0,0,0.1);
+      }
+
+      nav {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 1140px;
+        text-align: center;
+      }
+
+      ul {
+        display: flex;
+        justify-content: flex-end;
+      }
+
+      nav > ul {
+        padding: 4px 0;
+      }
+
+      li {
+        display: flex;
+        padding: 6px 8px;
+      }
+
+      li:last-child {
+        padding-right: 0;
+      }
+
+      a {
+        font-size: 15px;
+        text-decoration: none;
+        color: #111;
+        transition: .25s color ease-out;
+      }
+
+      a:hover {
+        color: #067df7;
+      }
+    `}</style>
   </header>
 )
 
