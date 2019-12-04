cc7f71e384e366622c389bff6bade3c6e253db0c
Alex Devero <deveroalex@gmail.com>
2019-12-04 10:49:54 +0100

    Add meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,48 @@ const Home = () => (
     <Head>
       <title>Trainr | Home</title>
 
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="handheldFriendly" content="true" />
+      {/* Maximum length 155 characters */}
+      <meta name="description" content="" />
+      <meta name="subject" content="Trainr | Home" />
+      <meta name="language" content="English" />
+      <meta name="robots" content="index,follow" />
+      <meta name="googlebot" content="index,follow" />
+      <meta name="classification" content="business" />
+      <meta name="url" content="" />
+
+      {/* Favicons */}
       <link rel='icon' href='/favicon.ico' />
+      <link rel="apple-touch-icon" sizes="180x180" href="" />
+      <link rel="icon" type="image/png" href="" sizes="32x32" />
+      <link rel="icon" type="image/png" href="" sizes="16x16" />
+      <link rel="manifest" href="" />
+      <link rel="mask-icon" href="" color="#111" />
+      <link rel="shortcut icon" href="" />
+      <meta name="msapplication-config" content="" />
+      <meta name="theme-color" content="" />
+
+      {/* Facebook OpenGraph tags */}
+      <meta property="og:title" content="Trainr | Home" />
+      <meta property="og:type" content="" />
+      <meta property="og:description" content="" />
+      <meta property="og:image" content="" />
+      <meta property="og:url" content="" />
+      <meta property="og:locale" content="" />
+      <meta property="article:author" content="" />
+
+      {/* Twitter cards tags */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="" />
+      <meta name="twitter:creator" content="@alexdevero" />
+      <meta name="twitter:url" content="" />
+      <meta name="twitter:title" content="Trainr | Home" />
+      <meta name="twitter:description" content="" />
+      <meta name="twitter:image" content="" />
+
+      {/* Disable automatic detection and formatting of possible phone numbers */}
+      <meta name="format-detection" content="telephone=no" />
     </Head>
 
     <Nav />
