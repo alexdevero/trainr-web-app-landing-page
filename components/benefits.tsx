a75466e0ebe81f78b4058b192b3924a06c00cf51
Alex Devero <deveroalex@gmail.com>
2020-01-01 11:24:17 +0100

    Add container styles to beneftis component

--- components/benefits.tsx
+++ components/benefits.tsx
@@ -1,7 +1,19 @@
 import * as React from 'react'
 
 const Benefits = () => (
-  <section id="benefits"></section>
+  <section id="benefits">
+    <div className="container"></div>
+
+    <style jsx>{`
+      .container {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 1140px;
+      }
+    `}</style>
+  </section>
 )
 
 export default Benefits
