23609294fe90578554f7d0b8e8507a507073e132
Alex Devero <deveroalex@gmail.com>
2020-01-01 11:24:52 +0100

    Add container styles to how it works component

--- components/how-it-works.tsx
+++ components/how-it-works.tsx
@@ -43,6 +43,16 @@ const HowItWorks = () => (
         </div>
       </div>
     </div>
+
+    <style jsx>{`
+      .container {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 1140px;
+      }
+    `}</style>
   </section>
 )
 
