8b2caaf4068ef34a663d4c5618eefa5dbe584dbf
Alex Devero <deveroalex@gmail.com>
2020-01-01 11:25:41 +0100

    Add container styles to features component

--- components/features.tsx
+++ components/features.tsx
@@ -47,6 +47,16 @@ const Features = () => (
         </div>
       </div>
     </div>
+
+    <style jsx>{`
+      .container {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 1140px;
+      }
+    `}</style>
   </section>
 )
 
