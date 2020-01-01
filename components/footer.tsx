d4e0a9fad548eb0e836500b1059ddff736ace1b4
Alex Devero <deveroalex@gmail.com>
2020-01-01 16:04:11 +0100

    Update and restyle footer content

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,26 +4,66 @@ const Footer = () => (
   <footer>
     <div className="container">
       <ul>
-        <li><a href="">Home</a></li>
-        <li><a href="">FAQs</a></li>
-        <li><a href="">How to use</a></li>
+        {/* <li><a href="">Home</a></li> */}
+        {/* <li><a href="">FAQs</a></li> */}
+        {/* <li><a href="">How to use</a></li> */}
         <li><a href="">About</a></li>
         <li><a href="">Terms</a></li>
         <li><a href="">Sign up</a></li>
-        <li><a href="">Sign in</a></li>
-        <li><a href="">Contact</a></li>
+        {/* <li><a href="">Sign in</a></li> */}
+        {/* <li><a href="">Contact</a></li> */}
       </ul>
 
-      <p>Copyright &copy; 2019 Devero Technologies</p>
+      <p className="copyright">Copyright &copy; 2019 Devero Technologies</p>
     </div>
 
     <style jsx>{`
+      footer {
+        padding-top: 32px;
+        padding-bottom: 32px;
+        background: #222;
+      }
+
       .container {
         padding-right: 15px;
         padding-left: 15px;
         margin-right: auto;
         margin-left: auto;
         max-width: 1140px;
+      }
+
+      ul {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: 0;
+        padding: 0;
+      }
+
+      li {
+        list-style-type: none;
+      }
+
+      li + li {
+        margin-left: 18px;
+      }
+
+      li a {
+        font-size: 14px;
+        text-decoration: none;
+        color: rgba(255, 255, 255, .85);
+      }
+
+      li a:hover {
+        text-decoration: underline;
+      }
+
+      .copyright {
+        margin-top: 28px;
+        margin-bottom: 0;
+        font-size: 12px;
+        text-align: center;
+        color: rgba(255, 255, 255, .5);
       }
     `}</style>
   </footer>
