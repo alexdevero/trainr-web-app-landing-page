8b8c3448fb3d523516be904aa6997af15427d7f1
Alex Devero <deveroalex@gmail.com>
2020-01-01 11:25:04 +0100

    Add container styles to pricing component

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -124,6 +124,14 @@ const Pricing = () => (
     </div>
 
     <style jsx>{`
+      .container {
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+        margin-left: auto;
+        max-width: 1140px;
+      }
+
       .row {
         display: flex;
         flex-wrap: wrap;
