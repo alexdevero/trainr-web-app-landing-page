557d046ff67e481bee118f622347544233f7255f
Alex Devero <deveroalex@gmail.com>
2020-01-02 10:22:02 +0100

    Re-style hero cta button

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,24 +1,28 @@
 import * as React from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 
+import { colors } from '../constants/theme'
+
 const Hero = () => (
   <section className="hero">
     <div className="container">
       <div className="row">
-        <div className="col-6">
+        <div className="col-8">
           <div className="hero__content">
-            <h1 className="hero__title">Take your performance to the next level</h1>
+            <h1 className="hero__title">Train harder and smarter</h1>
+            {/* Take your performance to the next level */}
 
-            <h2 className="hero__subtitle">Plan, track and sustain your training progress. Beginner or advanced, Trainr will provide you with the tools and knowledge you need to crush CrossFit Leaderboard.</h2>
+            <h2 className="hero__subtitle">Trainr helps you plan, track and sustain your training progress.</h2>
+            {/* Beginner or advanced, Trainr gives you with the tools and knowledge you need to crush CrossFit Leaderboard. */}
             {/* If you are a beginner Trainr will help you get started with CrossFit. If you are experienced athlete, Trainr will help you up your game. */}
 
-            <AnchorLink href="#pricing"><span className="hero__cta">Start Now</span></AnchorLink>
+            <AnchorLink href="#pricing"><span className="hero__cta">Learn more</span></AnchorLink>
           </div>
         </div>
 
-        <div className="col-6">
+        {/* <div className="col-6">
           <img src="" alt=""/>
-        </div>
+        </div> */}
       </div>
     </div>
 
@@ -62,6 +66,30 @@ const Hero = () => (
         max-width: 1140px;
       }
 
+      .row {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        margin-right: -15px;
+        margin-left: -15px;
+      }
+
+      [class*=col-] {
+        padding-right: 15px;
+        padding-left: 15px;
+        width: 100%;
+      }
+
+      .col-6 {
+        flex: 0 0 50%;
+        max-width: 50%;
+      }
+
+      .col-8 {
+        flex: 0 0 66.666667%;
+        max-width: 66.666667%;
+      }
+
       .hero__content {
         text-align: center;
         color: #fff;
@@ -77,24 +105,27 @@ const Hero = () => (
         margin-top: 0;
         margin-bottom: 32px;
         font-size: 48px;
-        font-weight: 700;
+        font-weight: 500;
       }
 
       .hero__subtitle {
         margin-bottom: 32px;
         font-size: 32px;
-        font-weight: 300;
+        font-weight: 100;
       }
 
       .hero__cta {
         display: inline-block;
-        padding: 16px 44px;
+        padding: 12px 28px;
+        margin: 28px auto 0;
+        font-size: 16px;
         font-weight: 700;
         text-decoration: none;
-        color: #000;
-        background: #fff;
-        border: 1px solid #fff;
+        color: ${colors.white};
+        background: ${colors.primary};
+        border: 0;
         border-radius: 4px;
+        cursor: pointer;
       }
     `}</style>
   </section>
