294dc874f7120a3258bd7c4b0f9ffb2366ced1ef
Alex Devero <deveroalex@gmail.com>
2020-01-02 09:48:05 +0100

    Fix theme colors names

--- constants/theme.ts
+++ constants/theme.ts
@@ -3,7 +3,7 @@ export const colors = {
   black: '#222',
   gray: '#eee',
   primary: '#067df7',
-  textLight: '#253858',
-  textDark: '#091E42',
+  textHeadings: '#253858',
+  textText: '#091E42',
   white: '#fff'
 }
