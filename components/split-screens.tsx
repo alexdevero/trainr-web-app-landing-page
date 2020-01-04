a5a4e05ff1b2821c1814515799eaccff8c1946c0
Alex Devero <deveroalex@gmail.com>
2020-01-04 10:08:46 +0100

    Update texts

--- components/split-screens.tsx
+++ components/split-screens.tsx
@@ -3,45 +3,45 @@ import * as React from 'react'
 const SplitScreens = () => (
   <section id="splitScreens" className="split-screens">
     <div className="container">
-      <div className="row">
+      <div className="row split__row">
         <div className="col-md-6">
           <img className="split__image" src="/images/trainr-app-snapshots/trainr--dashboard.png" alt="" />
         </div>
 
         <div className="col-md-6">
-          <h2 className="h4">Track & visualize your workouts</h2>
+          <h2 className="h4">Track & measure what matters</h2>
 
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, nostrum? Cupiditate laudantium explicabo consequuntur, atque iusto ducimus quia ipsum deleniti, esse doloribus vero.</p>
+          <p>Trainr helps you track everything, from your workouts and PRs to your weight, calories, water intake, supplements, and body measurements. You can also add your own custom trackers.</p>
 
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, nostrum? Cupiditate laudantium explicabo consequuntur, atque iusto ducimus quia ipsum deleniti, esse doloribus vero.</p>
+          <p>You can preview all these data in various types of detailed and comprehensive graphs and visualization tools. This will help you find out where you can improve your performance.</p>
         </div>
       </div>
 
-      <div className="row">
+      <div className="row split__row">
         <div className="col-md-6">
-          <h2 className="h4">Measure what matters</h2>
+          <h2 className="h4">Challenge yourself</h2>
 
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, nostrum? Cupiditate laudantium explicabo consequuntur, atque iusto ducimus quia ipsum deleniti, esse doloribus vero.</p>
+          <p>Trainr has a big library of exercises for all body parts, equipment, you can choose from. This will help you keep challenging yourself and making continual progress.</p>
 
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, nostrum? Cupiditate laudantium explicabo consequuntur, atque iusto ducimus quia ipsum deleniti, esse doloribus vero.</p>
+          <p>There are also pre-made workouts and daily WODs that will help you keep the bar high. If that's still not enough, you can also create your own workout routines.</p>
+        </div>
+
+        <div className="col-md-6">
+          <img className="split__image" src="/images/trainr-app-snapshots/trainr--exercise-library.png" alt="" />
+        </div>
+      </div>
+
+      <div className="row split__row">
+        <div className="col-md-6">
+          <h2 className="h4">Get fit and have fun</h2>
+
+          <p>Whether you're a beginner, intermediate, or advanced, Trainr makes it easy to start with CrossFit and get fit. Most importantly, Trainr makes it easier to stay that way.</p>
+
+          <p>Make Trainr your personal trainer, get better, faster and stronger, and achieve all your fitness and health goals.</p>
         </div>
 
         <div className="col-md-6">
           <img className="split__image" src="/images/trainr-app-snapshots/trainr--measurements.png" alt="" />
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-md-6">
-          <img className="split__image" src="/images/trainr-app-snapshots/trainr--exercise-library.png" alt="" />
-        </div>
-
-        <div className="col-md-6">
-          <h2 className="h4">Choose from hundreds of exercises</h2>
-
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, nostrum? Cupiditate laudantium explicabo consequuntur, atque iusto ducimus quia ipsum deleniti, esse doloribus vero.</p>
-
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate, nostrum? Cupiditate laudantium explicabo consequuntur, atque iusto ducimus quia ipsum deleniti, esse doloribus vero.</p>
         </div>
       </div>
     </div>
@@ -78,10 +78,18 @@ const SplitScreens = () => (
         }
       }
 
+      .split__row {
+        align-itemS: center;
+      }
+
       .split__image {
         max-width: 100%;
         width: 100%;
         object-fit: contain;
+      }
+
+      h2 {
+        margin-top: 0;
       }
     `}</style>
   </section>
